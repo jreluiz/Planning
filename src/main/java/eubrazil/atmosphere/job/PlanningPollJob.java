dd3b15c48926153057967ee4d7e240085797a088
Jorge Luiz <jre-jl@hotmail.com>
2019-09-30 23:39:38 -0300

    Testing privacy rules

--- src/main/java/eubrazil/atmosphere/job/PlanningPollJob.java
+++ src/main/java/eubrazil/atmosphere/job/PlanningPollJob.java
@@ -1,10 +1,10 @@
 package eubrazil.atmosphere.job;
 
-import java.util.Date;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.kie.api.runtime.StatelessKieSession;
 import org.quartz.DisallowConcurrentExecution;
@@ -21,14 +21,15 @@ import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.stereotype.Component;
 
-import eubr.atmosphere.tma.entity.qualitymodel.Attribute;
 import eubr.atmosphere.tma.entity.qualitymodel.CompositeAttribute;
 import eubr.atmosphere.tma.entity.qualitymodel.ConfigurationProfile;
-import eubr.atmosphere.tma.entity.qualitymodel.Preference;
+import eubr.atmosphere.tma.entity.qualitymodel.HistoricalData;
 import eubr.atmosphere.tma.entity.qualitymodel.PrivacyObject;
 import eubr.atmosphere.tma.entity.qualitymodel.Rule;
 import eubr.atmosphere.tma.entity.qualitymodel.TrustworthinessObject;
 import eubr.atmosphere.tma.utils.ListUtils;
+import eubr.atmosphere.tma.utils.TreeUtils;
+import eubrazil.atmosphere.config.appconfig.PropertiesManager;
 import eubrazil.atmosphere.config.quartz.SchedulerConfig;
 import eubrazil.atmosphere.qualitymodel.SpringContextBridge;
 import eubrazil.atmosphere.service.TrustworthinessService;
@@ -45,19 +46,16 @@ public class PlanningPollJob implements Job {
 
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-	private static final Integer TRUSTWORTHINESS_CONFIGURATION_PROFILE_ID = 1;
-	
 	@Value("${trigger.job.time}")
 	private String triggerJobTime;
-	
-	private static Date lastTimestampRead = null;
 
 	@Override
 	public void execute(JobExecutionContext jobExecutionContext) {
 		LOGGER.info("PlanningPollJob - execution..");
 
 		TrustworthinessService trustworthinessService = SpringContextBridge.services().getTrustworthinessService();
-		List<ConfigurationProfile> configProfileList = trustworthinessService.findConfigurationProfileInstance(TRUSTWORTHINESS_CONFIGURATION_PROFILE_ID);
+		Integer trustworthinessConfigProfileID = Integer.parseInt(PropertiesManager.getInstance().getProperty("trustworthiness_configuration_profile_id"));
+		List<ConfigurationProfile> configProfileList = trustworthinessService.findConfigurationProfileInstance(trustworthinessConfigProfileID);
 
 		if (ListUtils.isEmpty(configProfileList)) {
 			LOGGER.error("Quality Model is not defined in the database.");
@@ -67,36 +65,49 @@ public class PlanningPollJob implements Job {
 		ConfigurationProfile configurationActor =  ListUtils.getFirstElement(configProfileList);
 		LOGGER.info("TrustworthinessQualityModel (TrustworthinessPollJob) - ConfigurationProfile: " + configurationActor);
 		
-		CompositeAttribute trustworthiness = getRootAttribute(configurationActor);
+		CompositeAttribute trustworthiness = TreeUtils.getInstance().getRootAttribute(configurationActor);
 		
 		// building drools rules dynamically
 		trustworthiness.initRules();
-		Map<CompositeAttribute, Set<Rule>> compositeRules = trustworthiness.buildRules(PrivacyObject.class.getName());
+		Map<CompositeAttribute, List<Rule>> compositeRules = trustworthiness.buildRules(PrivacyObject.class.getName());
 		
 		Iterator<?> it = compositeRules.entrySet().iterator();
 	    while (it.hasNext()) {
 	        Map.Entry pair = (Map.Entry) it.next();
 	        LOGGER.info(pair.getKey() + " = " + pair.getValue());
 	        
-	        Set<Rule> rules = (Set<Rule>) pair.getValue();
-	        
-	        //AdaptationManager.performAdaptation(@{actions});
-	        
-//	        for (Rule rule : rules) {
-//	        	System.out.println(rule.getConditions());
-//				System.out.println(rule.getActions());
-//			}
+	        List<Rule> rules = (List<Rule>) pair.getValue();
 	        
 	        //Create a session to operate Drools in memory
 			DroolsUtility utility = new DroolsUtility();
 			try {
 				StatelessKieSession session = utility.loadSession(rules,
-						"eubrazil/atmosphere/templates/Trustworthiness.drl");
+						PropertiesManager.getInstance().getProperty("template_rules"));
+				
+				CompositeAttribute ca = (CompositeAttribute) pair.getKey();
+				List<HistoricalData> historicalDataList = ca.getPreference().getAttribute().getHistoricaldata();
+				
+				// sort historical data list by instant
+				Comparator<HistoricalData> compareByInstant = (HistoricalData h1, HistoricalData h2) -> h1.getId()
+						.getInstant().compareTo(h2.getId().getInstant());
+				Collections.sort(historicalDataList, compareByInstant);
+				
+				// get last historical data element
+				HistoricalData lastHistoricalData = ListUtils.getLastElement(historicalDataList);
+				// get second last historical data element
+				HistoricalData secondLastHistoricalData = null;
+				try { 
+					secondLastHistoricalData = historicalDataList.get(ListUtils.size(historicalDataList) - 2);
+				} catch (IndexOutOfBoundsException e) {
+					LOGGER.info("Historical data has not second last element.");
+				}
 				
 				TrustworthinessObject to = new PrivacyObject();
-				to.setScore(0.5);
-				to.setThreshold(0.6);
-				((PrivacyObject) to).setPreviousScore(0.4);
+				to.setScore(lastHistoricalData.getValue());
+				to.setThreshold(ca.getPreference().getThreshold());
+				if (secondLastHistoricalData != null) {
+					((PrivacyObject) to).setPreviousScore(secondLastHistoricalData.getValue());
+				}
 				
 				session.setGlobal("trustworthinessObject", to);
 				session.execute(to);
@@ -109,16 +120,6 @@ public class PlanningPollJob implements Job {
 	    }
 		
 		LOGGER.info("PlanningPollJob - end of execution..");
-	}
-	
-	private CompositeAttribute getRootAttribute(ConfigurationProfile configurationActor) {
-		for (Preference preference : configurationActor.getPreferences()) {
-			Attribute attr = preference.getAttribute();
-			if ( attr.getAttributeType().isRoot() ) {
-				return (CompositeAttribute) preference.getAttribute();
-			}
-		}
-		return null;
 	}
 	
 	@Bean(name = "jobBean1")
