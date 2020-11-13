43426dd4e5d4db029bfe346d8e01ca49b3cece56
JorgeLuiz <jre-jl@hotmail.com>
2020-11-13 19:25:24 -0300

    Adicionando comentários.

--- src/main/java/eubrazil/atmosphere/job/PlanningPollJob.java
+++ src/main/java/eubrazil/atmosphere/job/PlanningPollJob.java
@@ -160,8 +160,7 @@ public class PlanningPollJob implements Job {
 			}
 		}
 		
-		// TODO Verify that all rules (of attributes) have been executed (RULE ATTRIBUTE STATUS), and change the PLAN STATUS
-		
+		// Verify that all rules (of attributes) have been executed (RULE ATTRIBUTE STATUS), and change the PLAN STATUS
 		//execute children rules
 		if (attr instanceof CompositeAttribute) {
 			List<Attribute> children = ((CompositeAttribute) attr).getChildren();
